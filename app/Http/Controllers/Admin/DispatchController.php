e9cf1340dc9fde09773ffdfd72970e785cb0d996
MACBOOK <macbook@MacBook-Air-MACBOOK.local>
2026-02-12 22:23:25 +0700

    Update pdf

--- app/Http/Controllers/Admin/DispatchController.php
+++ app/Http/Controllers/Admin/DispatchController.php
@@ -15,7 +15,7 @@ class DispatchController extends Controller
 {
     public function index()
     {
-        $dispatches = Dispatch::with(['driver','ambulance','logs'])
+        $dispatches = Dispatch::with(['driver','ambulance'])
             ->orderByDesc('created_at')
             ->get();
 
