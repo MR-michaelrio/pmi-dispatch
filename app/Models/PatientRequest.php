10e2f8b26fb9854d5e80fdfebed1a6cf8917ecb1
MACBOOK <macbook@MacBook-Air-MACBOOK.local>
2026-02-12 22:30:47 +0700

    Update navbar

--- app/Models/PatientRequest.php
+++ app/Models/PatientRequest.php
@@ -4,9 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PatientRequest extends Model
 {
+    use SoftDeletes;
     protected $fillable = [
         'patient_name',
         'service_type',
